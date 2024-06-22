16e94e4c90364b74e5aad0bcafb5a2bbd85b09db
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-06-22 17:51:15 +0300

    Whithout JSX

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	return (
-		// Далее по коду начинается декларативный подход
-		// с использованием JSX ситаксиса
-		// где мы сразу пишем результат который хотим получить
-		// не прибегая к отдельному созданию элементов,
-		// назначению классов, атрибутов и внутреннему содержимому.
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>{new Date().getFullYear()}</span>
-			</header>
-		</div>
+	const nowYear = new Date().getFullYear();
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload 2.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('span', null, nowYear),
+		),
 	);
 };
