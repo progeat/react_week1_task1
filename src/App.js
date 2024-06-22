2a38511e5a68b3960af5f084b051759b1d373aec
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-06-22 17:38:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+	return (
+		// Далее по коду начинается декларативный подход
+		// с использованием JSX ситаксиса
+		// где мы сразу пишем результат который хотим получить
+		// не прибегая к отдельному созданию элементов,
+		// назначению классов, атрибутов и внутреннему содержимому.
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload 2.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<span>{new Date().getFullYear()}</span>
+			</header>
+		</div>
+	);
+};
